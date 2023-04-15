ab79e9130afced0c5338bfb7d8533f7faaaa94e7
Higor de Deus <higordeus22@gmail.com>
2023-04-15 03:20:18 -0300

    "Bug das cores de fundo resolvido"

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   VStack,
@@ -10,9 +10,13 @@ import {
   Text,
   Divider,
   useMediaQuery,
-  Flex,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
 } from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { SunIcon, MoonIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { RiBarChartBoxLine } from "react-icons/ri";
 import { AiOutlineHome, AiFillLayout } from "react-icons/ai";
 
@@ -24,6 +28,9 @@ const Sidebar = ({
 }) => {
   const { colorMode, toggleColorMode } = useColorMode();
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   const renderIndicator = (page) =>
     currentPage === page ? (
@@ -37,74 +44,124 @@ const Sidebar = ({
       />
     ) : null;
 
-    const renderButton = (text, icon, onClick, page) => (
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          width="100%"
-          onClick={onClick}
-        >
-          <Button
-            leftIcon={<Icon as={icon} boxSize="16px" />}
-            variant="ghost"
-            fontSize="sm"
-            fontWeight="semibold"
-            textAlign="left"
-          >
-            {text}
-          </Button>
-          {renderIndicator(page)}
-        </Flex>
-      );
-      
+  const renderButton = (text, icon, onClick, page) => (
+    <Button
+      key={text}
+      leftIcon={<Icon as={icon} boxSize="16px" />}
+      variant="ghost"
+      fontSize="sm"
+      fontWeight="semibold"
+      textAlign="left"
+      onClick={onClick}
+      width="100%"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Text flex="1">{text}</Text>
+      {renderIndicator(page)}
+    </Button>
+  );
 
   return (
-    <Box
-      position="fixed"
-      left="0"
-      top="0"
-      width={isLargerThan768 ? "220px" : "100%"}
-      height="100%"
-      bg={colorMode === "dark" ? "#111C44" : "white"}
-      padding="4"
-      boxShadow="md"
-      zIndex="1"
-    >
-      <VStack spacing="4" alignItems="stretch">
-        <Heading size="lg" mb="2" textAlign="center" fontSize="md">
-          NETMASTER
-        </Heading>
-        <Divider borderColor="gray.300" my={2} />
-        {renderButton(
-          "Presentation",
-          AiOutlineHome,
-          onPresentationClick,
-          "presentation"
-        )}
-        {renderButton(
-          "Control Panel",
-          AiFillLayout,
-          onControlPanelClick,
-          "control-panel"
-        )}
-        {renderButton(
-          "Dashboard",
-          RiBarChartBoxLine,
-          onDashboardClick,
-          "dashboard"
-        )}
-        <Spacer />
-        <Button
-          aria-label="Toggle Dark Mode"
-          variant="ghost"
-          leftIcon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-          onClick={toggleColorMode}
+    <>
+      {isLargerThan768 ? (
+        <Box
+          position="fixed"
+          left="0"
+          top="0"
+          width="220px"
+          height="100%"
+          bg={colorMode === "dark" ? "#111C44" : "white"}
+          padding="4"
+          boxShadow="md"
+          zIndex="1"
         >
-          {colorMode === "dark" ? "Light Mode" : "Dark Mode"}
-        </Button>
-      </VStack>
-    </Box>
+          <VStack spacing="4" alignItems="stretch">
+            <Heading size="lg" mb="2" textAlign="center" fontSize="md">
+              NETMASTER
+            </Heading>
+            <Divider borderColor="gray.300" my={2} />
+            {renderButton(
+              "Presentation",
+              AiOutlineHome,
+              onPresentationClick,
+              "presentation"
+            )}
+            {renderButton(
+              "Control Panel",
+              AiFillLayout,
+              onControlPanelClick,
+              "control-panel"
+            )}
+            {renderButton(
+              "Dashboard",
+              RiBarChartBoxLine,
+              onDashboardClick,
+              "dashboard"
+            )}
+            <Spacer />
+            <Button
+              aria-label="Toggle Dark Mode"
+              variant="ghost"
+              leftIcon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+              onClick={toggleColorMode}
+            >
+              {colorMode === "dark" ? "Light Mode" : "Dark Mode"}
+            </Button>
+          </VStack>
+        </Box>
+      ) : (
+        <>
+          <IconButton
+            icon={<HamburgerIcon />}
+            onClick={toggleMenu}
+            position="fixed"
+            zIndex="2"
+            bg={colorMode === "dark" ? "#111C44" : "white"}
+            boxShadow="md"
+          />
+          <Drawer isOpen={isMenuOpen} onClose={toggleMenu} placement="left" size="xs">
+            <DrawerOverlay />
+            <DrawerContent bg={colorMode === "dark" ? "#111C44" : "white"}>
+              <DrawerCloseButton />
+              <VStack spacing="4" alignItems="stretch" padding="4">
+                <Heading size="lg" mb="2" textAlign="center" fontSize="md">
+                  NETMASTER
+                </Heading>
+                <Divider borderColor="gray.300" my={2} />
+                {renderButton(
+                  "Presentation",
+                  AiOutlineHome,
+                  onPresentationClick,
+                  "presentation"
+                )}
+                {renderButton(
+                  "Control Panel",
+                  AiFillLayout,
+                  onControlPanelClick,
+                  "control-panel"
+                )}
+                {renderButton(
+                  "Dashboard",
+                  RiBarChartBoxLine,
+                  onDashboardClick,
+                  "dashboard"
+                )}
+                <Spacer />
+                <Button
+                  aria-label="Toggle Dark Mode"
+                  variant="ghost"
+                  leftIcon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+                  onClick={toggleColorMode}
+                >
+                  {colorMode === "dark" ? "Light Mode" : "Dark Mode"}
+                </Button>
+              </VStack>
+            </DrawerContent>
+          </Drawer>
+        </>
+      )}
+    </>
   );
 };
-
 export default Sidebar;
