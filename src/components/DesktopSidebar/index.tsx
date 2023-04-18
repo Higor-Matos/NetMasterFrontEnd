ef14434e0779a18515dfd92ecbb834828d0ec001
Higor de Deus <higordeus22@gmail.com>
2023-04-18 01:53:27 -0300

    "Código organizado e dividido"

---
+++ src/components/DesktopSidebar/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, VStack, Heading, Divider, Spacer, useColorMode } from "@chakra-ui/react";
+import { MenuButton } from "../index";
+
+type DesktopSidebarProps = {
+  menuItems: any[];
+  currentPage: string;
+  renderIndicator: (page: string) => JSX.Element;
+};
+
+const DesktopSidebar = ({ menuItems, currentPage, renderIndicator }: DesktopSidebarProps) => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      display={{ base: "none", md: "block" }}
+      position="fixed"
+      left="0"
+      top="0"
+      width="220px"
+      height="100%"
+      bg={colorMode === "dark" ? "#111C44" : "white"}
+      padding="4"
+      boxShadow="md"
+      zIndex="1"
+      borderRadius="md"
+    >
+      <VStack spacing="2" alignItems="stretch">
+        <Heading size="sm" mb="1" textAlign="center" fontSize="lg">
+          NET MASTER
+        </Heading>
+        <Divider borderColor="gray.300" my={2} />
+        {menuItems.map((item) => (
+          <MenuButton
+            key={item.text}
+            text={item.text}
+            icon={item.icon}
+            onClick={item.onClick}
+            page={item.page}
+            renderIndicator={renderIndicator}
+          />
+        ))}
+        <Spacer />
+      </VStack>
+    </Box>
+  );
+};
+
+export default DesktopSidebar;
---
+++ src/components/SidebarDrawer/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  VStack,
+  Heading,
+  Divider,
+  useColorMode,
+  Spacer
+} from "@chakra-ui/react";
+import { MenuButton } from "../index";
+
+type SidebarDrawerProps = {
+  menuItems: any[];
+  isOpen: boolean;
+  onClose: () => void;
+  currentPage: string;
+  renderIndicator: (page: string) => JSX.Element;
+};
+
+const SidebarDrawer = ({
+  menuItems,
+  isOpen,
+  onClose,
+  currentPage,
+  renderIndicator
+}: SidebarDrawerProps) => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} placement="left" size="xs">
+      <DrawerOverlay />
+      <DrawerContent bg={colorMode === "dark" ? "#111C44" : "white"} borderRadius="md">
+        <DrawerCloseButton />
+        <VStack spacing="4" alignItems="stretch" padding="4">
+          <Heading size="lg" mb="2" textAlign="center" fontSize="md">
+            NET MASTER
+          </Heading>
+          <Divider borderColor="gray.300" my={2} />
+          {menuItems.map((item) => (
+            <MenuButton
+              key={item.text}
+              text={item.text}
+              icon={item.icon}
+              onClick={item.onClick}
+              page={item.page}
+              renderIndicator={renderIndicator}
+            />
+          ))}
+          <Spacer />
+        </VStack>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default SidebarDrawer;
