37e7df1bc795d78ecbee24385fdb903f4eca5277
Higor de Deus <higordeus22@gmail.com>
2023-04-23 19:49:50 -0300

    "Sombras inseridas nas seções"

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -17,6 +17,10 @@ const Section: React.FC<SectionProps> = ({ title, children }) => {
   const backgroundColor = useColorModeValue("gray.100", "gray.900");
   const borderColor = useColorModeValue("gray.200", "gray.700");
   const textColor = useColorModeValue("gray.800", "white");
+  const boxShadow = useColorModeValue(
+    "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    "0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.06)"
+  );
 
   return (
     <Box
@@ -29,6 +33,7 @@ const Section: React.FC<SectionProps> = ({ title, children }) => {
       flexDirection="column"
       flex="1"
       minHeight="100%"
+      boxShadow={boxShadow}
     >
       <Heading as="h3" fontSize="lg" mb={4} color={textColor}>
         {title}
