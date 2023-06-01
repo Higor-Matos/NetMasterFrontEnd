1ec30d7d72e8d227ad3c269506410b2a9dc58dce
Higor de Deus <higordeus22@gmail.com>
2023-06-01 10:03:01 -0300

    "Componente estilizado"

--- src/components/RamChart/index.tsx
+++ src/components/RamChart/index.tsx
@@ -1,19 +1,82 @@
 import React from "react";
-import { PieChart, Pie, Tooltip, Cell, ResponsiveContainer } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Cell,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+import { FaMemory } from "react-icons/fa";
+import { useColorModeValue } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 interface RamChartProps {
   ramData: { name: string; value: number }[];
 }
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const COLORS = ["#E85D75", "#56B4A9", "#FFA24A", "#5A77DB"];
 
 const RamChart: React.FC<RamChartProps> = ({ ramData }) => {
   if (ramData.length === 0) {
-    return <div>Sem dados disponíveis para exibição.</div>;
+    return <Box p="4">Sem dados disponíveis para exibição.</Box>;
   }
 
+  const iconColor = useColorModeValue("#283142", "#F5F5F5");
+  const chartBgColor = useColorModeValue("#F5F5F5", "#283142");
+  const labelColor = useColorModeValue("#283142", "#F5F5F5");
+
+  const total = ramData.reduce((sum, data) => sum + data.value, 0);
+  const renderTooltipContent = (props: any) => {
+    const { payload } = props;
+    if (payload && payload.length > 0) {
+      const data = payload[0].payload;
+      const percentage = (data.value / total) * 100;
+      return (
+        <div
+          style={{
+            backgroundColor: chartBgColor,
+            padding: "10px",
+            border: "1px solid #ccc",
+            boxShadow: "0px 3px 6px rgba(0,0,0,0.16)",
+            borderRadius: "4px",
+          }}
+        >
+          <p
+            style={{
+              color: "#8884d8",
+              marginBottom: "5px",
+              fontWeight: "bold",
+            }}
+          >{`${data.value} GB`}</p>
+          <p style={{ color: labelColor }}>{`${percentage.toFixed(2)}%`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
-    <div>
+    <Box
+      boxShadow="0px 3px 6px rgba(0,0,0,0.16)"
+      p="4"
+      borderRadius="lg"
+      backgroundColor={chartBgColor}
+    >
+      <h2
+        style={{
+          color: labelColor,
+          textAlign: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "xl",
+          fontWeight: "bold",
+        }}
+      >
+        <FaMemory style={{ marginRight: "8px", color: iconColor }} />
+        Uso de Memória RAM
+      </h2>
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
           <Pie
@@ -23,7 +86,7 @@ const RamChart: React.FC<RamChartProps> = ({ ramData }) => {
             cx="50%"
             cy="50%"
             outerRadius={80}
-            fill="#8884d8"
+            fill={chartBgColor}
             label
           >
             {ramData.map((entry, index) => (
@@ -33,10 +96,11 @@ const RamChart: React.FC<RamChartProps> = ({ ramData }) => {
               />
             ))}
           </Pie>
-          <Tooltip />
+          <Tooltip content={renderTooltipContent} />
+          <Legend />
         </PieChart>
       </ResponsiveContainer>
-    </div>
+    </Box>
   );
 };
 
